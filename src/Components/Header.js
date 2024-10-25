f0c80ce76e86bc63e731aa939e4406ecdbc7963a
ashutoshqsofte <ashutosh@qsofte.com>
2024-10-25 16:00:37 +0530

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <>
+      <div className="head"></div>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/Pages/Home.js
@@ -0,0 +1,11 @@
+import "./Page.css";
+
+function Home() {
+  return (
+    <>
+      <div className="home-page-main"></div>
+    </>
+  );
+}
+
+export default Home;
